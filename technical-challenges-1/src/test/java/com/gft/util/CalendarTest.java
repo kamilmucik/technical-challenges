87436a51bd53e87dc3ea622ffe3f5c00d4261bd8
Kamil Mucik <kamu@gft.com>
2016-10-19 11:46:50 +0200

    test refactor

--- technical-challenges-1/src/test/java/com/gft/util/CalendarTest.java
+++ technical-challenges-1/src/test/java/com/gft/util/CalendarTest.java
@@ -1,42 +1,12 @@
 package com.gft.util;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Assert;
-import org.junit.Assume;
-import org.junit.Before;
 import org.junit.Test;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.text.ParseException;
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.Iterator;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-/**
- * Created by kamu on 2016-10-18.
- */
 public class CalendarTest {
-
-    static final Logger LOG = LoggerFactory.getLogger(CalendarTest.class);
-
-    private Calendar calendar;
-
-    @Before
-    public void setup() {
-        calendar = new Calendar(prepare());
-    }
-
-    @Test
-    public void getLocalDateTest() {
-        Assume.assumeNotNull(calendar);
-        assertTrue(calendar.getLocalDate() instanceof LocalDate);
-    }
 
     @Test
     public void shouldReturnWellKnowValues(){
@@ -51,21 +21,5 @@ public class CalendarTest {
         Assertions.assertThat(it.next()).isEqualTo(LocalDate.of(2016, 9, 23));
         Assertions.assertThat(it.next()).isEqualTo(LocalDate.of(2016, 9, 27));
 
-    }
-
-    @Test (expected = UnsupportedOperationException.class)
-    public void removeFromIteratorTest(){
-        Assume.assumeNotNull(calendar);
-        calendar.iterator().remove();
-    }
-
-    @Test
-    public void formatTest() throws ParseException {
-        Assume.assumeNotNull(calendar);
-        Assert.assertEquals("2016/IX/19" ,Calendar.FORMATTER.format(calendar.getDate()) );
-    }
-
-    private LocalDate prepare() {
-        return LocalDate.parse("2016/IX/19", Calendar.FORMATTER);
     }
 }
