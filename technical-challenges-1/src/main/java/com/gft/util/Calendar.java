a1e43d918e660cf4622ff2cdd6d5056f821d8f13
Kamil Mucik <kamu@gft.com>
2016-10-19 15:07:00 +0200

    test refactor

--- technical-challenges-1/src/main/java/com/gft/util/Calendar.java
+++ technical-challenges-1/src/main/java/com/gft/util/Calendar.java
@@ -2,13 +2,7 @@ package com.gft.util;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.format.SignStyle;
-import java.time.temporal.ChronoField;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
 
 /**
  * Created by kamu on 2016-10-18.
@@ -17,59 +11,26 @@ public class Calendar implements Iterable<LocalDate>{
 
     private LocalDate date;
 
-    public static final Map<Long, String> ROMAN_MONTHS = new HashMap<Long, String>();
-
-    public static final DateTimeFormatter FORMATTER;
-
-    static {
-        ROMAN_MONTHS.put(1L, "I");
-        ROMAN_MONTHS.put(2L, "II");
-        ROMAN_MONTHS.put(3L, "III");
-        ROMAN_MONTHS.put(4L, "IV");
-        ROMAN_MONTHS.put(5L, "V");
-        ROMAN_MONTHS.put(6L, "VI");
-        ROMAN_MONTHS.put(7L, "VII");
-        ROMAN_MONTHS.put(8L, "VIII");
-        ROMAN_MONTHS.put(9L, "IX");
-        ROMAN_MONTHS.put(10L, "X");
-        ROMAN_MONTHS.put(11L, "XI");
-        ROMAN_MONTHS.put(12L, "XII");
-
-        FORMATTER = new DateTimeFormatterBuilder()
-                .appendValue(ChronoField.YEAR, 4)
-                .appendLiteral('/')
-                .appendText(ChronoField.MONTH_OF_YEAR, ROMAN_MONTHS)
-                .appendLiteral('/')
-                .appendValue(ChronoField.DAY_OF_MONTH, 1, 2, SignStyle.NORMAL)
-                .toFormatter();
-    }
-
     public Calendar(LocalDate localDate){
         this.date = localDate;
     }
 
-    public LocalDate getLocalDate() {
-        return LocalDate.now();
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
     @Override
     public Iterator<LocalDate> iterator() {
-        return new CalendarIterator<LocalDate>();
+        return new CalendarIterator(date);
     }
 
-    private class CalendarIterator <LocalDate> implements Iterator<java.time.LocalDate> {
+    private static class CalendarIterator implements Iterator<LocalDate> {
+
+        private LocalDate date;
+
+        public CalendarIterator(LocalDate date){
+            this.date = date;
+        }
 
         /**
          * Method always return true, because calendar is infinity.
-         * @return
+         * @return true
          */
         public boolean hasNext() {
             return true;
@@ -79,20 +40,13 @@ public class Calendar implements Iterable<LocalDate>{
          * Method next() return every <i>Tuesday</i> and <i>Friday</i> in week days.
          * @return
          */
-        public java.time.LocalDate next() {
+        public LocalDate next() {
             do {
-                setDate(date.plusDays(1));
+                this.date = date.plusDays(1);
             } while ( (!date.getDayOfWeek().equals(DayOfWeek.TUESDAY))
                     && (!date.getDayOfWeek().equals(DayOfWeek.FRIDAY)));
 
             return date;
-        }
-
-        /**
-         * No one can remove date form calendar
-         */
-        public void remove() {
-            throw new UnsupportedOperationException();
         }
     }
 }
--- technical-challenges-1/src/test/java/com/gft/util/CalendarTest.java
+++ technical-challenges-1/src/test/java/com/gft/util/CalendarTest.java
@@ -17,9 +17,19 @@ public class CalendarTest {
         Iterator<LocalDate> it = cal.iterator();
 
         // assert
+        Assertions.assertThat(it.hasNext()).isTrue();
         Assertions.assertThat(it.next()).isEqualTo(LocalDate.of(2016, 9, 20));
         Assertions.assertThat(it.next()).isEqualTo(LocalDate.of(2016, 9, 23));
         Assertions.assertThat(it.next()).isEqualTo(LocalDate.of(2016, 9, 27));
+    }
 
+    @Test
+    public void shouldReturnIndependentIterators(){
+        Calendar cal = new Calendar(LocalDate.of(2016, 9, 19));
+
+        LocalDate date1 = cal.iterator().next();
+        LocalDate date2 = cal.iterator().next();
+
+        Assertions.assertThat(date1).isEqualTo(date2);
     }
 }
