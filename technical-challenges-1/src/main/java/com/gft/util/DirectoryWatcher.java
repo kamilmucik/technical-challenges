9c78d3e2cd7ecc63ad3db60b31ceb40c9571f470
Kamil Mucik <kamil.mucik@gft.com>
2016-11-17 18:23:12 +0100

    create file watcher with test

--- technical-challenges-1/src/main/java/com/gft/util/DirectoryWatcher.java
+++ technical-challenges-1/src/main/java/com/gft/util/DirectoryWatcher.java
@@ -1,4 +1,186 @@
 package com.gft.util;
 
-public class DirectoryWatcher {
+import com.gft.model.Node;
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+import rx.Observable;
+import rx.Scheduler;
+import rx.Subscriber;
+import rx.schedulers.Schedulers;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+final class DirectoryWatcher {
+
+
+    static Builder newBuilder() {
+        return new Builder();
+    }
+
+    private static class FileSystemEventOnSubscribe implements Observable.OnSubscribe<Node> {
+        private WatchService watcher;
+        private Scheduler scheduler;
+        private volatile boolean close = false;
+        private Node rootNode;
+        private Path path;
+        private  WatchEvent.Kind[] eventKind = new WatchEvent.Kind[]{
+                StandardWatchEventKinds.ENTRY_CREATE,
+                StandardWatchEventKinds.ENTRY_MODIFY,
+                StandardWatchEventKinds.ENTRY_DELETE
+        };
+
+        public FileSystemEventOnSubscribe(Node rootNode, Scheduler scheduler){
+            try {
+                this.rootNode = rootNode;
+                this.scheduler = scheduler;
+                //this.watcher = FileSystems.getDefault().newWatchService();
+                path = ((Path) rootNode.getPayload());
+                this.watcher = path.getFileSystem().newWatchService();
+
+                path.register(this.watcher,eventKind);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void call(Subscriber<? super Node> subscriber) {
+//            WatchEvent.Kind kind = event.kind();
+//            if (StandardWatchEventKinds.OVERFLOW != kind) {
+//                this.path = (Path) event.context();
+//            }
+
+//            System.out.println((Path)rootNode.getPayload());
+            getEventsForCurrentFiles((Path)rootNode.getPayload()).forEach(event -> {
+                WatchEvent.Kind kind = event.kind();
+                subscriber.onNext(rootNode);
+            });
+
+
+            Scheduler.Worker worker = scheduler.createWorker();
+            subscriber.add(worker);
+
+            worker.schedule(() -> {
+
+                do {
+
+                    try {
+
+                        WatchKey key = watcher.take();
+                        System.out.println("close: " + close);
+                        System.out.println("key: " + key);
+                        if (key == null) {
+                            continue;
+                        }
+//
+                        for (WatchEvent<?> event : key.pollEvents()) {
+//                            FileSystemEvent fileSystemEvent = new FileSystemEvent(event);
+//                            System.out.println(event.kind());
+
+                            subscriber.onNext(rootNode);
+                        }
+//
+                        if (!key.reset()) {
+                            close();
+                        }
+                    } catch (Throwable t) {
+                        t.printStackTrace();
+//                        subscriber.onError(t);
+                    }
+                } while (!close);
+                subscriber.onCompleted();
+            });
+        }
+
+        public void close() {
+            this.close = true;
+
+            try {
+                watcher.close();
+            } catch (Exception e) {}
+        }
+
+        private List<WatchEvent<Path>> getEventsForCurrentFiles(Path directory) {
+            final List<WatchEvent<Path>> events = new ArrayList<>();
+            try {
+                Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
+                        events.add(pathToWatchEvent(path));
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes attrs) {
+                        events.add(pathToWatchEvent(path));
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return events;
+        }
+
+        private WatchEvent<Path> pathToWatchEvent(Path path) {
+            return new WatchEvent<Path>() {
+                @Override
+                public Kind<Path> kind() {
+                    return StandardWatchEventKinds.ENTRY_CREATE;
+                }
+
+                @Override
+                public int count() {
+                    return 1;
+                }
+
+                @Override
+                public Path context() {
+                    return path;
+                }
+            };
+        }
+    }
+
+
+    static class Builder {
+
+        private Node rootNode;
+
+
+
+        private Scheduler scheduler = Schedulers.newThread();
+
+        Builder() {}
+
+        Builder addPath(Node node){
+            rootNode = node;
+
+            return this;
+        }
+
+        Observable<Node> build(){
+            FileSystemEventOnSubscribe fileSystemEventOnSubscribe
+                    = new FileSystemEventOnSubscribe(rootNode, scheduler);
+
+//            Observable<Node> fileSystemEventObservable
+//                    = NodeConverter.convert(new TreeConverter(rootNode));
+
+            Observable<Node> fileSystemEventObservable
+                    = Observable.create(fileSystemEventOnSubscribe);
+
+
+            fileSystemEventObservable
+                    .doOnUnsubscribe(fileSystemEventOnSubscribe::close);
+
+            return fileSystemEventObservable;
+        }
+
+    }
+
 }
--- technical-challenges-1/src/test/java/com/gft/util/DirectoryWatcherIT.java
+++ technical-challenges-1/src/test/java/com/gft/util/DirectoryWatcherIT.java
@@ -1,14 +1,60 @@
 package com.gft.util;
 
+import com.gft.model.Node;
+import com.gft.model.NodeImpl;
+import com.google.common.collect.ImmutableList;
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
 import org.junit.Test;
+import rx.Observable;
+import rx.Subscription;
+import rx.observers.TestSubscriber;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.CountDownLatch;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class DirectoryWatcherIT {
 
 
-    @Test
-    public void shouldReturnChangesInPath(){
+    @Test(timeout = 15_000)
+    public void shouldReturnChangesInPath() throws IOException, InterruptedException {
+        FileSystem fs = Jimfs.newFileSystem(Configuration.unix());
+        Path home = fs.getPath("/data");
+        Files.createDirectory(home);
+        Node<Path> rootNode = new NodeImpl<>(home);
+        CountDownLatch latch = new CountDownLatch(1);
+        Observable<Node> fileSystemWatcher =
+                DirectoryWatcher
+                        .newBuilder()
+                        .addPath(rootNode)
+                        .build();
+
+        TestSubscriber subscriber = new TestSubscriber();
+        Subscription subscribe = fileSystemWatcher
+                .doOnNext(a -> {
+                    System.out.println("Got an event for " + a.getPayload());
+                })
+                .subscribe(subscriber);
+
+        fileSystemWatcher.subscribe(System.out::println);
 
 
+        Path onePath = home.resolve("test.txt");
+        Files.write(onePath, ImmutableList.of("1"), StandardCharsets.UTF_8);
+
+        Thread.sleep(3000);
+
+//        latch.await();
+
+        subscribe.unsubscribe();
+
+        assertThat(subscribe.isUnsubscribed()).isTrue();
 
     }
 }
